8128e249f716f4bc71d9d12e2cb55bb7b4c2c806
Samuel Noble <enoble2009@gmail.com>
2022-08-13 14:38:54 -0300

    Adding first domain classes.

---
+++ WinterJam2022/Assets/WinterJam2022/Scripts/Cards/Domain/Card.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card
+{
+
+    public int points;
+    public string word;
+    public string description;
+
+}
---
+++ WinterJam2022/Assets/WinterJam2022/Scripts/Rounds/Domain/Round.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Round
+{
+    
+    public PlayerRoundInfo player1;
+    public PlayerRoundInfo player2;
+
+
+}
